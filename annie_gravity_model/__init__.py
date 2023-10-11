f4e046dbfad871b6a384955b4432f5ce262d9a16
Will Dickson <wbd@caltech.edu>
2023-10-11 15:18:32 -0700

    Cleaned up a bit more. Added utility module.

--- annie_gravity_model/__init__.py
+++ annie_gravity_model/__init__.py
@@ -1,6 +1,2 @@
 from .gravity_model import GravityModel
-from .gravity_model import load_datasets
-#from .gravity_model import sort_data_files_by_alpha
-#from .gravity_model import get_list_of_data_files 
-#from .gravity_model import plot_filtered_forces
-#from .gravity_model import plot_grab_sections
+from .utility import load_datasets
