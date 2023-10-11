754b8fd6c8b6183e51b5ce5ba83c63f18819e3fe
Will Dickson <wbd@caltech.edu>
2023-10-10 17:38:57 -0700

    Added plot_filtered_forces to __init__

--- annie_gravity_model/__init__.py
+++ annie_gravity_model/__init__.py
@@ -1,3 +1,4 @@
 from .gravity_model import GravityModel
 from .gravity_model import sort_data_files_by_alpha
 from .gravity_model import get_list_of_data_files 
+from .gravity_model import plot_filtered_forces
