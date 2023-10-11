dc5ccb6180d532cacdc501c065df47214b55210a
Will Dickson <wbd@caltech.edu>
2023-10-11 15:07:45 -0700

    Moved data loading to separate function.

--- annie_gravity_model/gravity_model.py
+++ annie_gravity_model/gravity_model.py
@@ -15,7 +15,7 @@ class GravityModel:
     Model of gravitational forces acting on Robofly wing.  Use for gravitation
     force subtraction when analysing data from a set of polars. 
     """
-    DEFAULT_FIT_PARAM = {
+    DEFAULT_DATA_PRM = {
             'v'       : 10,   # velocity (int) in kinematics filename
             'xi'      :  0,   # xi value (int) in kinematics filenames  
             't_lim'   : None, # (tuple) lower/upper limits of time range 
@@ -24,7 +24,7 @@ class GravityModel:
             'num_phi' : 50,   # number of phi data points after resampling
             }
 
-    DEFAULT_PLT_PARAM = {
+    DEFAULT_PLOT_PRM = {
             'force_surfaces'  : False,
             'filtered_forces' : False, 
             'grab_sections'   : False,
@@ -112,10 +112,10 @@ class GravityModel:
         plot_force_surfaces(eta, phi, fx, fy, fz)
 
 
-    def fit(self, data_dir, fit_param=None, plt_param=None): 
+    def fit(self, data_dir, data_prm=None, plot_prm=None): 
         """
-        Fit gravitational model using data polar from data_dir and parameters
-        in fit_param.
+        Fit gravitational model using data polar from data_dir and prmeters
+        in data_prm.
 
         Arguments:
 
@@ -123,8 +123,7 @@ class GravityModel:
 
         keyword Arguments:
 
-            fit_param = dictionary containing fit parameters
-
+            data_prm = dictionary containing fit prmeters
             {
                 'v'       :  # velocity (int) in kinematics filenames
                 'xi'      :  # xi value (int) in kinematics filenames
@@ -133,7 +132,7 @@ class GravityModel:
                 'num_phi' :  # number of phi data points after resampling
             }
 
-            plt_param = dictionary of plotting options.
+            plot_prm = dictionary of plotting options.
             {
                 'force_surfaces'   : True
                 'force_filter'     : False, 
@@ -143,25 +142,25 @@ class GravityModel:
 
         """
         # Set default options and update with user options
-        _fit_param = dict(self.DEFAULT_FIT_PARAM)
-        if fit_param is not None:
-            _fit_param.update(fit_param)
+        _data_prm = dict(self.DEFAULT_DATA_PRM)
+        if data_prm is not None:
+            _data_prm.update(data_prm)
 
-        _plt_param = dict(self.DEFAULT_PLT_PARAM)
-        if plt_param is not None:
-            _plt_param.update(plt_param)
+        _plot_prm = dict(self.DEFAULT_PLOT_PRM)
+        if plot_prm is not None:
+            _plot_prm.update(plt_prm)
 
         # Read in datasets and extract force sections
-        datasets = self.extract_datasets(data_dir, _fit_param, _plt_param)
+        datasets = load_datasets(data_dir, _data_prm, _plot_prm)
 
         # Create fx, fy, and fz force surfaces as functions of eta and phi (meshgrids)
-        self.model['force_surface'] = self.create_force_surfaces(datasets, _fit_param)
+        self.model['force_surface'] = self.create_force_surfaces(datasets, _data_prm)
         
         # Create the force interpolators for fx, fy, fz
         self.model['force_interp'] = self.create_force_interpolators(self.model['force_surface'])
-        self.model['fit_param'] = fit_param 
+        self.model['data_prm'] = data_prm 
 
-        if _plt_param['force_surfaces']:
+        if _plot_prm['force_surfaces']:
             self.plot_force_surfaces(plot_type='data')
 
 
@@ -186,14 +185,14 @@ class GravityModel:
 
 
 
-    def create_force_surfaces(self, datasets, fit_param):
+    def create_force_surfaces(self, datasets, data_prm):
         """ 
         Creates surfaces for fx, fy, fz forces as functions of eta and phi 
 
         Arguments:
 
           datasets   = dictionary mapping eta values to kinematics and forces. 
-          fit_param  = dictionary of fitting parameters 
+          data_prm  = dictionary of fitting prmeters 
 
         Returns:
 
@@ -206,12 +205,12 @@ class GravityModel:
         """
         # Get arrays of eta and phi values 
         eta_pts = np.array(sorted(datasets.keys()))
-        eta_min, eta_max = fit_param['eta_lim']
+        eta_min, eta_max = data_prm['eta_lim']
         eta_mask = np.logical_and(eta_pts >= eta_min, eta_pts <= eta_max) 
         eta_pts = eta_pts[eta_mask]
         phi_max = min([datasets[eta]['phi'].max() for eta in eta_pts])
         phi_min = max([datasets[eta]['phi'].min() for eta in eta_pts])
-        phi_pts = np.linspace(phi_min, phi_max, fit_param['num_phi'])
+        phi_pts = np.linspace(phi_min, phi_max, data_prm['num_phi'])
 
         # Create meshgrid
         eta, phi = np.meshgrid(eta_pts, phi_pts)
@@ -240,128 +239,125 @@ class GravityModel:
         return surface_data
 
 
-    def extract_datasets(self, data_dir, fit_param, plt_param): 
-        """ 
-        Extract data sections with dphi=constant for each eta 
-
-        Arguments:
-          
-          data_dir = location of kinematics data files
-          fit_param = dictionary containing fit parameters
-          plt_param = dictionary containing plotting options
-
-        """
-        data_files = get_list_of_data_files(data_dir, v=fit_param['v'], xi=fit_param['xi'])
-        alphas, data_files = sort_data_files_by_alpha(data_files)
-        datasets = {}
-
-        print('loading datasets')
-        for alpha, file in zip(alphas, data_files):
-            print(f'  {file}')
-            data = io.loadmat(str(file))
-
-            # Extract the data we need for polars
-            t = data['t_FT_s'][:,0]
-            eta = data['wingkin_s'][:,2]
-            phi = data['wingkin_s'][:,3]
-            dphi = data['wingkin_s'][:,9]
-            fx = data['FT_conv_s'][0,:]
-            fy = data['FT_conv_s'][2,:]
-            fz = data['FT_conv_s'][1,:]
-
-            # Cut out sections between t_lim[0] and t_lim[1]
-
-            if fit_param['t_lim'] is not None:
-                mask_t_lim = np.logical_and(t >= fit_param['t_lim'][0], t <= fit_param['t_lim'][1])
-                t = t[mask_t_lim]
-                eta = eta[mask_t_lim]
-                phi = phi[mask_t_lim]
-                dphi = dphi[mask_t_lim]
-                fx = fx[mask_t_lim]
-                fy = fy[mask_t_lim]
-                fz = fz[mask_t_lim]
-
-            # Lowpass filter force data
-            dt = t[1] - t[0]
-            force_filt = sig.butter(4, fit_param['fcut'], btype='low', output='ba', fs=1/dt)
-            fx_filt = sig.filtfilt(*force_filt, fx)
-            fy_filt = sig.filtfilt(*force_filt, fy)
-            fz_filt = sig.filtfilt(*force_filt, fz)
-
-            # Optional plot showing filtered and unfilterd force data
-            if plt_param['filtered_forces']:
-                plot_filtered_forces(t, fx, fy, fz, fx_filt, fy_filt, fz_filt, alpha)
-
-            # Get sections where dphi is equal to maximum
-            mask_pos = dphi >= dphi.max() 
-            t_pos = t[mask_pos]
-            eta_pos = eta[mask_pos]
-            phi_pos = phi[mask_pos]
-            dphi_pos = dphi[mask_pos]
-            fx_filt_pos = fx_filt[mask_pos]
-            fy_filt_pos = fy_filt[mask_pos]
-            fz_filt_pos = fz_filt[mask_pos]
-
-            # Get sections where dphi is equal to -maximum
-            mask_neg = dphi <= dphi.min() 
-            t_neg = t[mask_neg]
-            eta_neg = eta[mask_neg]
-            phi_neg = phi[mask_neg]
-            dphi_neg = dphi[mask_neg]
-            fx_filt_neg = fx_filt[mask_neg]
-            fy_filt_neg = fy_filt[mask_neg]
-            fz_filt_neg = fz_filt[mask_neg]
-
-            # Save datasets as function of eta
-            eta_pos_val = eta_pos.max()
-            eta_neg_val = eta_neg.min()
-            
-            datasets[eta_pos_val] = { 
-                    't'    : t_pos, 
-                    'eta'  : eta_pos,
-                    'phi'  : phi_pos, 
-                    'dphi' : dphi_pos,
-                    'fx'   : fx_filt_pos,
-                    'fy'   : fy_filt_pos,
-                    'fz'   : fz_filt_pos, 
-                    }
-
-            datasets[eta_neg_val] = { 
-                    't'    : t_neg, 
-                    'eta'  : eta_neg,
-                    'phi'  : phi_neg, 
-                    'dphi' : dphi_neg,
-                    'fx'   : fx_filt_neg,
-                    'fy'   : fy_filt_neg,
-                    'fz'   : fz_filt_neg, 
-                    }
-
-
-            # Optional plot showing grab sections for gravitational model
-            if plt_param['grab_sections']:
-                datafull = {
-                        't'   : t, 
-                        'eta' : eta, 
-                        'phi' : phi, 
-                        'dphi': dphi, 
-                        'fx'  : fx_filt, 
-                        'fy'  : fy_filt, 
-                        'fz'  : fz_filt
-                        }
-                plot_grab_sections(datafull, datasets[eta_pos_val], datasets[eta_neg_val], alpha)
-
-            if plt_param['force_pos_neg']:
-                plot_force_pos_neg(datasets[eta_pos_val], datasets[eta_neg_val], alpha)
-
-        return  datasets
-
-
-
-
-
-
 # Utility functions
 # ---------------------------------------------------------------------------------------
+
+def load_datasets(data_dir, data_prm, plot_prm): 
+    """ 
+    Extract data sections with dphi=constant for each eta 
+
+    Arguments:
+      
+      data_dir = location of kinematics data files
+      data_prm = dictionary containing fit prmeters
+      plot_prm = dictionary containing plotting options
+
+    """
+    data_files = get_list_of_data_files(data_dir, v=data_prm['v'], xi=data_prm['xi'])
+    alphas, data_files = sort_data_files_by_alpha(data_files)
+    datasets = {}
+
+    print('loading datasets')
+    for alpha, file in zip(alphas, data_files):
+        print(f'  {file}')
+        data = io.loadmat(str(file))
+
+        # Extract the data we need for polars
+        t = data['t_FT_s'][:,0]
+        eta = data['wingkin_s'][:,2]
+        phi = data['wingkin_s'][:,3]
+        dphi = data['wingkin_s'][:,9]
+        fx = data['FT_conv_s'][0,:]
+        fy = data['FT_conv_s'][2,:]
+        fz = data['FT_conv_s'][1,:]
+
+        # Cut out sections between t_lim[0] and t_lim[1]
+
+        if data_prm['t_lim'] is not None:
+            mask_t_lim = np.logical_and(t >= data_prm['t_lim'][0], t <= data_prm['t_lim'][1])
+            t = t[mask_t_lim]
+            eta = eta[mask_t_lim]
+            phi = phi[mask_t_lim]
+            dphi = dphi[mask_t_lim]
+            fx = fx[mask_t_lim]
+            fy = fy[mask_t_lim]
+            fz = fz[mask_t_lim]
+
+        # Lowpass filter force data
+        dt = t[1] - t[0]
+        force_filt = sig.butter(4, data_prm['fcut'], btype='low', output='ba', fs=1/dt)
+        fx_filt = sig.filtfilt(*force_filt, fx)
+        fy_filt = sig.filtfilt(*force_filt, fy)
+        fz_filt = sig.filtfilt(*force_filt, fz)
+
+        # Optional plot showing filtered and unfilterd force data
+        if plot_prm['filtered_forces']:
+            plot_filtered_forces(t, fx, fy, fz, fx_filt, fy_filt, fz_filt, alpha)
+
+        # Get sections where dphi is equal to maximum
+        mask_pos = dphi >= dphi.max() 
+        t_pos = t[mask_pos]
+        eta_pos = eta[mask_pos]
+        phi_pos = phi[mask_pos]
+        dphi_pos = dphi[mask_pos]
+        fx_filt_pos = fx_filt[mask_pos]
+        fy_filt_pos = fy_filt[mask_pos]
+        fz_filt_pos = fz_filt[mask_pos]
+
+        # Get sections where dphi is equal to -maximum
+        mask_neg = dphi <= dphi.min() 
+        t_neg = t[mask_neg]
+        eta_neg = eta[mask_neg]
+        phi_neg = phi[mask_neg]
+        dphi_neg = dphi[mask_neg]
+        fx_filt_neg = fx_filt[mask_neg]
+        fy_filt_neg = fy_filt[mask_neg]
+        fz_filt_neg = fz_filt[mask_neg]
+
+        # Save datasets as function of eta
+        eta_pos_val = eta_pos.max()
+        eta_neg_val = eta_neg.min()
+        
+        datasets[eta_pos_val] = { 
+                't'    : t_pos, 
+                'eta'  : eta_pos,
+                'phi'  : phi_pos, 
+                'dphi' : dphi_pos,
+                'fx'   : fx_filt_pos,
+                'fy'   : fy_filt_pos,
+                'fz'   : fz_filt_pos, 
+                }
+
+        datasets[eta_neg_val] = { 
+                't'    : t_neg, 
+                'eta'  : eta_neg,
+                'phi'  : phi_neg, 
+                'dphi' : dphi_neg,
+                'fx'   : fx_filt_neg,
+                'fy'   : fy_filt_neg,
+                'fz'   : fz_filt_neg, 
+                }
+
+
+        # Optional plot showing grab sections for gravitational model
+        if plot_prm['grab_sections']:
+            datafull = {
+                    't'   : t, 
+                    'eta' : eta, 
+                    'phi' : phi, 
+                    'dphi': dphi, 
+                    'fx'  : fx_filt, 
+                    'fy'  : fy_filt, 
+                    'fz'  : fz_filt
+                    }
+            plot_grab_sections(datafull, datasets[eta_pos_val], datasets[eta_neg_val], alpha)
+
+        if plot_prm['force_pos_neg']:
+            plot_force_pos_neg(datasets[eta_pos_val], datasets[eta_neg_val], alpha)
+
+    return  datasets
+
+
 def get_list_of_data_files(data_dir, v=10, xi=0):
     """ Get list of relvant data files - filter by v and xi. """
     data_dir = pathlib.Path(data_dir)
--- examples/fit_and_save_model.py
+++ examples/fit_and_save_model.py
@@ -1,14 +1,14 @@
 import pathlib
 from annie_gravity_model import GravityModel
 
-fit_param = {
+data_prm = {
         't_lim'   : ( 0.8, 7.5),  # t limits for finding grab sections
         'eta_lim' : ( -80,  80),  # eta limits for force surfaces
         }
 
 data_dir = pathlib.Path('../../results_with_conv')
 gravity_model = GravityModel()
-gravity_model.fit(data_dir, fit_param=fit_param)
+gravity_model.fit(data_dir, data_prm=data_prm)
 gravity_model.save('gravity_model.pkl')
 #gravity_model.plot_force_surfaces(plot_type='interp')
 
