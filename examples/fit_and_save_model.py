e49f7fd473acfa3594b168936090086d3233ecb7
Will Dickson <wbd@caltech.edu>
2023-10-11 17:53:43 -0700

    Fixed another issue with ind during data loading.

--- examples/fit_and_save_model.py
+++ examples/fit_and_save_model.py
@@ -5,10 +5,13 @@ data_prm = {
         't_lim'   : ( 0.8, 7.5),  # t limits for finding grab sections
         'eta_lim' : ( -80,  80),  # eta limits for force surfaces
         }
+plot_prm = {
+        'grab_sections' : True,
+        }
 
 data_dir = pathlib.Path('../../results_with_conv')
 gravity_model = GravityModel()
-gravity_model.fit(data_dir, data_prm=data_prm)
+gravity_model.fit(data_dir, data_prm=data_prm, plot_prm=plot_prm)
 gravity_model.save('gravity_model.pkl')
 #gravity_model.plot_force_surfaces(plot_type='interp')
 
